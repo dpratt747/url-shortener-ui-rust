76aa13499ce5ecb368259e26cc00df4ddecebfe3
David Pratt <dpratt747@gmail.com>
2025-06-03 22:31:50 +0100

    working form submission

--- src/app.rs
+++ src/app.rs
@@ -6,28 +6,21 @@ use yew::prelude::*;
 pub fn app() -> Html {
     let url_state = use_state(|| String::new());
 
-    // let on_submit_url = {
-    //     let url_state = url_state.clone();
-    //     move |ev: SubmitEvent| {
-    //         ev.prevent_default();
-    //         console::log_1(&"submission pressed".into());
-    //         // let value = "New URL".to_string();
-    //         // url_state.set(value);
-    //         web_sys::window().unwrap().alert_with_message(&format!("URL Submitted: {}", *url_state)).unwrap();
-    //     }
-    // };
-
-    let on_submit_url = {
+    let on_submit_url = |field_name: String| -> Callback<SubmitEvent> {
         let url_state = url_state.clone();
         Callback::from(move |ev: SubmitEvent| {
             ev.prevent_default();
             if let Some(value) = web_sys::window().unwrap()
                 .document().unwrap()
-                .get_element_by_id("urlInput")
+                .get_element_by_id(&field_name)
                 .and_then(|el| el.dyn_ref::<HtmlInputElement>().map(|input| input.value()))
             {
+                console::log_1(&"Input successfully retrieved.".into());
                 url_state.set(value.clone());
-                // web_sys::window().unwrap().alert_with_message(&format!("URL Submitted: {}", value.clone())).unwrap();
+
+                // todo: make a shorten url request and return the shortened url
+
+                // web_sys::window().unwrap().alert_with_message(&format!("URL Submitted: {}", value.clone())).unwrap(); for debugging
             }
         })
     };
@@ -38,11 +31,10 @@ pub fn app() -> Html {
             <div>{ (*url_state).clone() }</div>
 
             <div>
-                <form onsubmit={on_submit_url}>
+                <form onsubmit={on_submit_url("urlInput".to_string())}>
                     <div class="mb-3">
                         <label for="urlInput" class="form-label fw-bold">{ "Enter a URL:" }</label>
                         <input class="form-control mb-2" type="url" id="urlInput" name="urlInput" placeholder="Enter a long URL here" style="width: 100%;" required=true/>
-                        // <input class="form-control mb-2" type="url" id="urlInput" name="urlInput" placeholder="Enter a long URL here" style="width: 100%;" required=true oninput={on_input_url} />
                         <button class="btn btn-outline-success" type="submit">{ "Submit" }</button>
                     </div>
                 </form>
